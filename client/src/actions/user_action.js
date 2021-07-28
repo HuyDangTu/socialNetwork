fe625de0e3e202722671fbc16b348448ef9838a6
HuyDangTu <66896891+HuyDangTu@users.noreply.github.com>
2021-07-28 22:28:00 +0700

    commit

--- client/src/actions/user_action.js
+++ client/src/actions/user_action.js
@@ -40,7 +40,7 @@ export function registerUser(dataToSubmit){
 }
 
 export function loginUser(dataToSubmit){
-    const request = axios.post(`${USER_SERVER}login`,dataToSubmit)
+    const request = axios.post(`${USER_SERVER}/login`,dataToSubmit)
     .then(response => response.data);
     console.log(request);
     return{
